47bb607c24db33e4b5122280fc1698bbd76a496e
grvelva <grvelva@student.21-school.ru>
2021-09-07 16:42:03 +0300

    ex02 added

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,62 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << CONS_MSG;
+	this->_value = 0;
+}
+
+Fixed::Fixed(const Fixed &num)
+{
+	std::cout << COPY_MSG;
+	*this = num;
+}
+
+Fixed::Fixed(int num)
+{
+	this->_value = (num << this->_fb);
+}
+
+Fixed::Fixed(float f)
+{
+	this->_value = roundf(f * (1 << this->_fb));
+}
+
+Fixed& Fixed::operator=(Fixed const& num)
+{
+	std::cout << ASSIGN_MSG;
+	this->_value = num._value;
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << DES_MSG;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << GRB_MSG;
+	return (this->_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_value = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (((float)this->_value / (1 << this->_fb)));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_value >> this->_fb);
+}
+
+std::ostream &operator<<(std::ostream &stream, Fixed const &num)
+{
+	stream << num.toFloat();
+	return (stream);
+}
